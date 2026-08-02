fb4548d2ff31b92c4188eec30ff02bb8b544aab8
trigologiaa <augusto.ribero.15@gmail.com>
2026-08-01 21:51:56 -0300

    feat: add arguments of Product class in the model

--- src/main/java/com/trigologiaa/HardwareStore/domain/model/Product.java
+++ src/main/java/com/trigologiaa/HardwareStore/domain/model/Product.java
@@ -1,6 +1,24 @@
 package com.trigologiaa.HardwareStore.domain.model;
 
 public class Product {
+    private Integer code;
+    private String name;
+    private String brand;
+    private String category;
+    private Double price;
+    private Integer stock;
+    private String description;
+
     public Product() {
+    }
+
+    public Product(Integer code, String name, String brand, String category, Double price, Integer stock, String description) {
+        this.code = code;
+        this.name = name;
+        this.brand = brand;
+        this.category = category;
+        this.price = price;
+        this.stock = stock;
+        this.description = description;
     }
 }
--- src/test/java/com/trigologiaa/HardwareStore/domain/model/ProductTest.java
+++ src/test/java/com/trigologiaa/HardwareStore/domain/model/ProductTest.java
@@ -9,4 +9,18 @@ class ProductTest {
         Product product = new Product();
         assertNotNull(product, "The product initialized without arguments must not be null");
     }
+
+    @Test
+    void aProductWithArgumentsShouldBeCreatedCorrectly() {
+        Product fullProduct = new Product(
+                12345,
+                "Hammer Drill",
+                "DeWalt",
+                "Power Tools",
+                159.99,
+                25,
+                "20V drill with lithium battery"
+        );
+        assertNotNull(fullProduct, "The product initialized with arguments must not be null");
+    }
 }
